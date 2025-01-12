dd17f908c5631d178521212c5c86f6d5017d24cb
Minamaher-sw <mosadefmena185@gmail.com>
2025-01-12 13:25:58 +0200

    adding use of dialogue box alert ,prompt and confirm

--- javaScript.js
+++ javaScript.js
@@ -55,6 +55,34 @@ function exLoopStat()
     var array=["index1","index2",3,4,5]
     for( var item in array)
     {
-        document.write("<br>",item);
+        document.write("<br>",array[item]);
     }
+}
+
+/*                                              ********** 
+ *                                             Dialogue Boxes
+                                                ************      
+        o The simplest way  to direct output to a dialog box  
+
+ * */
+//Alert: 
+alert("clock ok to continue")
+
+//Prompt:The simplest way to interact with the user. 
+var name_1=prompt("Enter your name ","write here") ;
+alert("welcome  " + name_1) ;
+/*
+    Confirm: 
+    o displays a dialog box with two buttons: OK and Cancel. 
+    • If the user clicks on OK it will return true. 
+    • If the user clicks on the Cancel  it will return false.
+*/
+var chack =confirm("do you want confirm data");
+if(chack)
+{
+    alert("thanks sir")
+}
+else
+{
+    alert("good")
 }
