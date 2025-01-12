a59ca5f23e0d11dedc1836a654341cad9e728260
Minamaher-sw <mosadefmena185@gmail.com>
2025-01-12 12:51:31 +0200

    adding function exConSta and adding use of if condition

--- javaScript.js
+++ javaScript.js
@@ -2,3 +2,25 @@ function sayHello(){
 
     alert("Hello SIR");
 }
+/*
+    Control Statements that can be used are: 
+    1. Conditional Statements 
+        a.  if ….else 
+        b.  switch/case 
+    2.  Loop Statements 
+        a.  for 
+        b.  while 
+        c.  do…while
+*/ 
+function exCondStat()
+{
+    var temp =23 ;
+    if(temp > 20)
+    {
+        document.write("air is little cold <br>")
+    }
+    else
+    {
+        document.write("air is very cold <br>")
+    }
+}
