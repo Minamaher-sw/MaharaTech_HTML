ab5e7e91b584c0ad2f4be698acec62c693f3dbb9
Minamaher-sw <mosadefmena185@gmail.com>
2025-01-18 07:33:08 +0200

    solving for q1 of assignment

--- lab1.js
+++ lab1.js
@@ -7,3 +7,29 @@ function sum(num1,num2)
 {
     return num1+num2 ;
 }
+function checkTemp_1(temp)
+{
+    var ret=(temp >=30)?'Hot' :"Cold";
+    return ret;
+}
+
+function checkTemp_2(temp ,feel)
+{
+    var state;
+    if((temp > 25 && temp <30) &&(feel >25 && feel<30) )
+    {
+        state="noraml"
+    }
+    else if(temp <25 && feel <25)
+    {
+        state="Cold"
+    }
+    else if(temp >30 && feel >30)
+    {
+        state="Hot"
+    }
+    else {
+        state="Ambiguous, can\'t detect"
+    }
+    return state;
+}
